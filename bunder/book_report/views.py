1b739efcbb299e94dd3542d5e0d66489fe08e81e
koomchang <leahpar0401@gmail.com>
2022-06-21 13:17:22 +0900

    FIX : book_report model

--- bunder/book_report/views.py
+++ bunder/book_report/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Book
 
 # Create your views here.
+
+def write_report(request):
+    return render(request, "write_report.html")
+
+def create(request):
+    new_book = Book()
+    new_book.book_title = request.POST['title']

22387654fdf69e41fcdd38837f445e3bd99a7b78
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-06-23 00:49:40 +0900

    FEAT: 소모임 멤버 중복 추가 방지 및 소모임 생성 시에 소모임장 멤버 자동 추가(OWNER)

--- bunder/book_club/views.py
+++ bunder/book_club/views.py
@@ -16,16 +16,21 @@ def new(request):
         book_club = BookClub()
         book_club.owner = request.user
         book_club.club_name = request.POST["clubname"]
-        book_club.member_cnt = request.POST["number_of_member"]
+        book_club.member_total = request.POST["number_of_member"]
         book_club.image = request.POST["club_img"]
         book_club.category = request.POST["book_club_category"]
         book_club.description = request.POST["description"]
-        # print("request.data: ",request.POST)
+        book_club.save()
+
+        member, created = BookClubMember.objects.get_or_create(
+            club=get_object_or_404(BookClub, id=book_club.id),
+            user=request.user,
+            type=BookClubMember.type_enum[2][0] # 소모임장
+        )
 
         if request.POST.get('zoom_url', True):
             book_club.link = request.POST["zoom_url"]
 
-        book_club.save()
         return redirect('/bookclub/' + str(book_club.id))
 
 
@@ -34,15 +39,17 @@ def book_club_detail(request, bookclub_id):
     bookclub_id_json = json.dumps(book_club.id)
     return render(request, 'book_club/club_detail.html', {'book_club': book_club, 'bookclub_id': bookclub_id_json})
 
+
 @csrf_exempt
 def request_member(request):
     req = json.loads(request.body)
     bookclub_id = req['id']
     if request.method == "POST":
-        member = BookClubMember()
-        member.club = get_object_or_404(BookClub, id=bookclub_id)
-        member.user = request.user
-        member.save()
+        member, created = BookClubMember.objects.get_or_create(
+            club=get_object_or_404(BookClub, id=bookclub_id),
+            user=request.user,
+        )
+
         return JsonResponse({'member': model_to_dict(member)})
 
 

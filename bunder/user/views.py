b28d0c03fc7a787008430e3983941a56e61b394b
koomchang <leahpar0401@gmail.com>
2022-06-23 19:16:53 +0900

    FIX : Logout 시 인덱스 화면으로 이동

--- bunder/user/views.py
+++ bunder/user/views.py
@@ -65,7 +65,7 @@ def login(request):
 # 로그아웃
 def logout(request):
     auth.logout(request)
-    return HttpResponse("로그아웃")
+    return render(request, "base/index.html")
 
 # 비밀번호 수정
 @csrf_exempt

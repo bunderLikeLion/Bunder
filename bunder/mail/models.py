bfcfb3e2f6b51a56e0a8f9c02c3654f4ff979c84
koomchang <leahpar0401@gmail.com>
2022-06-27 04:34:31 +0900

    FIX : 쪽지 모델 created_at 필드 추가

--- bunder/mail/models.py
+++ bunder/mail/models.py
@@ -13,3 +13,4 @@ class Mail(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receivernick')
     content = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)

e10ca441c397c1e9c2865171079b4a7f40e2d01b
jihyoung9912 <jihyoung9912@gmail.com>
2022-06-27 22:32:39 +0900

    FIX : base.html 템플릿 수정

--- bunder/user/urls.py
+++ bunder/user/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('signup', views.register),
+    path('signup', views.register, name = 'register'),
     path('login', views.login),
-    path('logout', views.logout),
+    path('logout', views.logout, name = 'logout'),
     path('profile', views.profile, name="profile"),
     path('updatepassword', views.password_revise),
     path('updateprofile', views.profile_revise),

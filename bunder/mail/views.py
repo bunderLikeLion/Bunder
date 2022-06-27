9c59ff58aea7fbfa9bd3cbf4232ad007d81bf013
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-06-27 20:12:22 +0900

    FIX: mail url 수정

--- bunder/mail/views.py
+++ bunder/mail/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Mail
 from django.views.decorators.csrf import csrf_exempt
 from user.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 # Create your views here.
 
 def main(request):
-    check_receiver = Mail.objects.filter(user=request.user).values_list('receiver', flat=True).distinct()
-    receiver = User.objects.filter(id__in=[id for id in check_receiver])
-    each_mail = request.GET.get('each_mail')
-    return render(request, "mail/mail.html", {'receiver': receiver})
+    if request.method == 'GET':
+        if request.GET.get("receiver"):
+            return HttpResponse("이미 멤버로 추가된 유저입니다.")
+
+        else:
+            check_receiver = Mail.objects.filter(user=request.user).values_list('receiver', flat=True).distinct()
+            receiver = User.objects.filter(id__in=[id for id in check_receiver])
+            each_mail = request.GET.get('each_mail')
+            return render(request, "mail/mail.html", {'receiver': receiver})
 
 
 def send_mail(request):

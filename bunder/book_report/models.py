717676d28af5617a4eb19b7f167e61ff59f470f7
koomchang <leahpar0401@gmail.com>
2022-06-28 17:53:25 +0900

    FIX : 독후감 작성에서 image 파일만 업로드 되도록 수정

--- bunder/book_report/models.py
+++ bunder/book_report/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.core.validators import FileExtensionValidator
 
 User = settings.AUTH_USER_MODEL
 
